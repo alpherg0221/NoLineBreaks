b60d7ae64fd8829eda153f78d2a6128b6cd6ad44
alpherg0221 <78247593+alpherg0221@users.noreply.github.com>
2023-12-02 21:05:26 +0900

    Changed build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/NoLineBreaks/",
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: `src/assets/[name].[ext]`,
-        entryFileNames: `src/assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+        entryFileNames: `assets/[name].js`,
       }
     }
   }
